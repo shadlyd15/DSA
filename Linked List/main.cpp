c24ae0ae9149385328b469ca9fdd78f2d7a28308
shadlyd15 <shadlyd15@gmail.com>
2020-12-09 23:12:47 +0100

    Linked list

--- Linked List/main.cpp
+++ Linked List/main.cpp
@@ -27,12 +27,53 @@ public:
 		return node;
 	}
 
+	bool insert(int value, size_t n){
+		Node * node_n = head;
+		for (int i = 0; i < n; ++i){
+			node_n = node_n->next;
+		}
+
+		Node * node = new Node();
+		if (node){
+			node->value = value;
+			node->next = node_n->next;
+			node_n->next = node;
+		}
+		return node_n;
+	}
+
+
+	bool replace(int value, size_t n){
+		Node * node_n = head;
+		for (int i = 0; i < n; ++i){
+			node_n = node_n->next;
+		}
+		if (node_n){
+			node_n->value = value;
+		}
+		return node_n;
+	}
+
 	bool read(){
 
 	}
 
-	bool remove(){
+	bool remove(size_t n){
+		bool ret_val = false;
+		Node * node_left = head;
+		for (int i = 0; i < n; ++i){
+			node_left = node_left->next;
+		}
 
+		if (node_left){
+			Node * node_n = node_left->next;
+			node_left->next = node_n->next;
+			if (node_n){
+				ret_val = true;
+				delete node_n;
+			}
+		}
+		return ret_val;
 	}
 
 	bool print(){
@@ -57,7 +98,13 @@ int main(int argc, char const *argv[]){
 	for (int i = 0; i < 10; ++i){
 		list->insert(i);
 	}
+	list->insert(99, 5);
 
+	list->print();
+
+	cout << "=====================" << endl;
+
+	list->remove(5);
 	list->print();
 	// cout << "test" << endl;
 	return 0;
